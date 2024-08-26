466b63dab3b0edb3f1a23a1d2dd2712de83ad02d
Shaik Mastanbi <skmastanbi523@gmail.com>
2024-08-26 15:41:25 +0530

    add eureka server and registration done for usermanagement and corporateclient applications with eureka

--- corporateclient/src/main/java/com/corporateclient/CorporateclientApplication.java
+++ corporateclient/src/main/java/com/corporateclient/CorporateclientApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 
 @SpringBootApplication
 @EnableConfigurationProperties(value = {CorporateClientContactInfoDto.class})
+
 public class CorporateclientApplication {
 
 	public static void main(String[] args) {
